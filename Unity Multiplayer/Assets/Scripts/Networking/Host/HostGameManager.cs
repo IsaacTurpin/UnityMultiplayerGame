ff8860db7d9c4c92dc86cc58e86ff74cd837a1af
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-07-21 12:52:36 +0100

    Overhead player names

--- Unity Multiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Unity Multiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -19,7 +19,7 @@ public class HostGameManager : IDisposable
     private Allocation allocation;
     private string joinCode;
     private string lobbyId;
-    private NetworkServer networkServer;
+    public NetworkServer NetworkServer { get; private set; }
     private const int MaxConnections = 20;
     private const string GameSceneName = "Game";
 
@@ -76,7 +76,7 @@ public class HostGameManager : IDisposable
             return;
         }
 
-        networkServer = new NetworkServer(NetworkManager.Singleton);
+        NetworkServer = new NetworkServer(NetworkManager.Singleton);
 
         UserData userData = new UserData
         {
@@ -121,6 +121,6 @@ public class HostGameManager : IDisposable
             lobbyId = string.Empty;
         }
 
-        networkServer?.Dispose();
+        NetworkServer?.Dispose();
     }
 }
