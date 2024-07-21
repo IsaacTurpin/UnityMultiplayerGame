b2e6b323dbb4ceeb819212535619f98bfa5c7dd1
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-07-21 15:00:07 +0100

    Respawning Players

--- Unity Multiplayer/Assets/Scripts/Core/Player/Player.cs
+++ Unity Multiplayer/Assets/Scripts/Core/Player/Player.cs
@@ -4,16 +4,21 @@ using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
 using Unity.Collections;
+using System;
 
 public class Player : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [field: SerializeField] public Health Health {  get; private set; }
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
+
+    public static event Action<Player> OnPlayerSpawned;
+    public static event Action<Player> OnPlayerDespawned;
 
     public override void OnNetworkSpawn()
     {
@@ -22,11 +27,21 @@ public class Player : NetworkBehaviour
             UserData userData = HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
 
             PlayerName.Value = userData.userName;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if(IsOwner)
         {
             virtualCamera.Priority = ownerPriority;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if(IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this);
         }
     }
 }
