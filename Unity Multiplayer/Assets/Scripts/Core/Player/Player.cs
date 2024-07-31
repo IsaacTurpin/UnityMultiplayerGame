4cb8031192c86fcab3d9012135eaa0bc817584c7
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-07-31 15:05:16 +0100

    Minimap

--- Unity Multiplayer/Assets/Scripts/Core/Player/Player.cs
+++ Unity Multiplayer/Assets/Scripts/Core/Player/Player.cs
@@ -10,11 +10,13 @@ public class Player : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private SpriteRenderer minimapIconRenderer;
     [field: SerializeField] public Health Health {  get; private set; }
     [field: SerializeField] public CoinWallet Wallet { get; private set; }
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
+    [SerializeField] private Color ownerColour = Color.cyan;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
 
@@ -35,6 +37,8 @@ public class Player : NetworkBehaviour
         if(IsOwner)
         {
             virtualCamera.Priority = ownerPriority;
+
+            minimapIconRenderer.color = ownerColour;
         }
     }
 
